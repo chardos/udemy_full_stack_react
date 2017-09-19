70594bffb031087b5aff9866f70dea7207e23ef7
Richard Tan <richard.tan@crownbet.com.au>
2017-09-19 23:42:24 +1000

    Lecture 14

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import BooksList from './components/pages/BooksList';
+
 import {createStore} from 'redux';
 import reducers from './reducers/index';
 import { addToCart } from './actions/cartActions';
@@ -7,10 +12,11 @@ import { postBooks, deleteBook, updateBook } from './actions/booksActions';
 
 const store = createStore(reducers);
 
-store.subscribe(() => {
-    console.log('current state: ', store.getState());
-    // console.log('2nd book price: ', store.getState()[1].price);
-})
+render (
+    <Provider store={store}>
+        <BooksList />
+    </Provider>, document.getElementById('app')
+)
 
 // STEP 2 create and dispatch actions
 store.dispatch(postBooks(
@@ -30,15 +36,14 @@ store.dispatch(postBooks(
    ]
 ));
 
-
-store.dispatch(deleteBook({id: 1}));
-store.dispatch(updateBook({
-    id: 2,
-    title: 'learn react in 24H'
-}));
-
-// ======= CART ACTIONS =========
-
-store.dispatch(
-    addToCart([{id: 1}])
-);
+// store.dispatch(deleteBook({id: 1}));
+// store.dispatch(updateBook({
+//     id: 2,
+//     title: 'learn react in 24H'
+// }));
+//
+// // ======= CART ACTIONS =========
+//
+// store.dispatch(
+//     addToCart([{id: 1}])
+// );
